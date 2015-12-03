9b9876d225107420e6de28ee983bfb8071b5eeb4
lichunqiang <light-li@hotmail.com>
2015-12-03 21:56:26 +0800

    cs fix [skip ci]

--- SweetSubmitAsset.php
+++ SweetSubmitAsset.php
@@ -1,44 +1,56 @@
-<?php namespace light\widgets;
+<?php
+
+/*
+ * This file is part of the light/yii2-sweet-submit.
+ *
+ * (c) lichunqiang <light-li@hotmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace light\widgets;
 
 use Yii;
 use yii\helpers\Json;
 use yii\web\AssetBundle;
 use yii\web\View;
+
 /**
- * sweet-submit asset
+ * sweet-submit asset.
  *
- * @package light\widgets
  * @version 1.0.0
+ *
  * @author lichunqiang <light-li@hotmail.com>
  */
 class SweetSubmitAsset extends AssetBundle
 {
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public $js = ['yii.enhance.js'];
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public $depends = [
         'yii\web\YiiAsset',
     ];
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function init()
     {
         parent::init();
-        $this->sourcePath = __DIR__ . DIRECTORY_SEPARATOR . 'dist' . DIRECTORY_SEPARATOR;
+        $this->sourcePath = __DIR__.DIRECTORY_SEPARATOR.'dist'.DIRECTORY_SEPARATOR;
 
         $this->initLocale();
         $this->initOptions();
     }
 
     /**
-     * Init plugin optins
+     * Init plugin optins.
      */
     protected function initOptions()
     {
@@ -46,20 +58,20 @@ class SweetSubmitAsset extends AssetBundle
 
         $opts = [
             'confirmButtonText' => Yii::t('sweetsubmit', 'Ok'),
-            'cancelButtonText' => Yii::t('sweetsubmit', 'Cancel'),
+            'cancelButtonText'  => Yii::t('sweetsubmit', 'Cancel'),
         ];
         $opts = Json::encode($opts);
 
         $view->registerJs("yii.sweetSubmitOptions = $opts;", View::POS_END);
     }
     /**
-     * Init locale
+     * Init locale.
      */
     protected function initLocale()
     {
         Yii::$app->i18n->translations['sweetsubmit'] = [
-            'class' => 'yii\i18n\PhpMessageSource',
-            'basePath' => __DIR__ . DIRECTORY_SEPARATOR . 'messages',
+            'class'    => 'yii\i18n\PhpMessageSource',
+            'basePath' => __DIR__.DIRECTORY_SEPARATOR.'messages',
         ];
     }
 }
--- messages/zh-CN/sweetsubmit.php
+++ messages/zh-CN/sweetsubmit.php
@@ -1,5 +1,15 @@
 <?php
+
+/*
+ * This file is part of the light/yii2-sweet-submit.
+ *
+ * (c) lichunqiang <light-li@hotmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 return [
-    'Ok' => '确定',
+    'Ok'     => '确定',
     'Cancel' => '取消',
 ];
